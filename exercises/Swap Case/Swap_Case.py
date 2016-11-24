e9fbe6930781e0b570d2c2ea99ca163581fce047
Marti Kecskemeti <martikecskemeti@gmail.com>
2016-11-24 20:05:03 +0100

    reverse

---
+++ exercises/Swap Case/Swap_Case.py
@@ -0,0 +1,17 @@
+
+word = input("Enter a word: ")
+
+
+def swap(word):
+    swap_word = []
+    for ch in word:
+        if ch == ch.lower():
+            swap_word.append(ch.upper())
+        else:
+            ch == ch.upper()
+            swap_word.append(ch.lower())
+    return "".join(swap_word)
+
+print (swap(word))
+
+
